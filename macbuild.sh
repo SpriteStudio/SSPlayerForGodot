467612a9eb8ae8ac3cfc5f7f70bf32f997c3a0ba
Naruto TAKAHASHI <tnaruto@gmail.com>
2024-05-01 22:07:04 +0900

    update macbuild.sh.

--- macbuild.sh
+++ macbuild.sh
@@ -6,12 +6,27 @@ if [ $# -ge 1 ]; then
     _BUILD_ARCH=$1
 fi
 
+if ! declare -p CCACHE > /dev/null 2>&1; then
+    if type "sccache" > /dev/null; then
+        export CCACHE=sccache
+    elif type "ccache" > /dev/null; then
+        export CCACHE=ccache
+    fi
+    echo "set ${CCACHE} as CCACHE"
+    export _USE_DEFAULT_CCACHE=1
+fi
+
 pushd godot
 scons platform=osx arch=${_BUILD_ARCH} compiledb=yes custom_modules="../gd_spritestudio"
+
 rm -rf ./Godot.app
 cp -r misc/dist/osx_tools.app ./Godot.app
 mkdir -p Godot.app/Contents/MacOS
-cp bin/godot.osx.tools.$_BUILD_ARCH Godot.app/Contents/MacOS/Godot
+cp bin/godot.osx.tools.${_BUILD_ARCH} Godot.app/Contents/MacOS/Godot
 chmod +x Godot.app/Contents/MacOS/Godot 
 codesign --force --timestamp --options=runtime --entitlements misc/dist/osx/editor.entitlements -s - Godot.app
 popd
+
+if [[ _USE_DEFAULT_CCACHE -eq 1 ]]; then
+    unset CCACHE
+fi
