7108ffba1e6797d4ff7713692cba982ed81b0974
Naruto TAKAHASHI <tnaruto@gmail.com>
2024-05-02 22:38:45 +0900

    use the same variable name with windows.

--- macbuild.sh
+++ macbuild.sh
@@ -6,8 +6,11 @@ GODOT_BRANCH=$(git branch --show-current | sed -e "s/[\r\n]\+//g")
 GODOT_TAG=$(git describe --tags --abbrev=0 | sed -e "s/[\r\n]\+//g")
 popd > /dev/null
 
-declare -A args=(
+declare -A opts=(
+  # Godot Build Options
   ["arch"]=${HOST_ARCH}
+
+  # Build Options
   ["ccache"]="false"
   ["version"]="3.x"
 )
@@ -20,18 +23,18 @@ while (( $# > 0 )); do
     kv=(${(@s/:/)item})
     key=$kv[1]
     value=$kv[2]
-    args[$key]=$value
+    opts[$key]=$value
   fi
 done
 
-echo "arguments"
-for key in ${(k)args}; do
-  echo "  $key => $args[$key]"
+echo "options"
+for key in ${(k)opts}; do
+  echo "  $key => $opts[$key]"
 done
 echo ""
 
 # ccache
-if [[ ${args[ccache]} == "true" ]]; then
+if [[ ${opts[ccache]} == "true" ]]; then
     if ! declare -p CCACHE > /dev/null 2>&1; then
         if type "sccache" > /dev/null; then
             export CCACHE=sccache
@@ -50,41 +53,41 @@ if [[ -n $GODOT_BRANCH ]]; then
 elif [[ -n $GODOT_TAG ]]; then
     VERSION=${GODOT_TAG}
 else
-    VERSION=${args[version]}
+    VERSION=${opts[version]}
 fi
 echo "Godot Version: ${VERSION}"
 
 if [[ ${VERSION} = *"3."* ]]; then
     # 3.x
-    args[platform]="osx"
-    args[app_template]="osx_tools.app"
-    args[app_bin]="godot.osx.tools"
+    opts[platform]="osx"
+    opts[app_template]="osx_tools.app"
+    opts[app_bin]="godot.osx.tools"
 else
     # 4.x
-    args[platform]="macos"
-    args[app_template]="macos_tools.app"
-    args[app_bin]="godot.macos.editor"
+    opts[platform]="macos"
+    opts[app_template]="macos_tools.app"
+    opts[app_bin]="godot.macos.editor"
 fi
 
 pushd godot
 # enable ccache on Godot
-if [[ ${args[ccache]} == "true" ]]; then
-  git checkout platform/${args[platform]}/detect.py
-  git apply ../misc/ccache/${args[platform]}_ccache.patch
+if [[ ${opts[ccache]} == "true" ]]; then
+  git checkout platform/${opts[platform]}/detect.py
+  git apply ../misc/ccache/${opts[platform]}_ccache.patch
 fi
 
-scons platform=${args[platform]} arch=${args[arch]} compiledb=yes custom_modules="../gd_spritestudio"
+scons platform=${opts[platform]} arch=${opts[arch]} compiledb=yes custom_modules="../gd_spritestudio"
 
 /bin/rm -rf ./Godot.app
-/bin/cp -r misc/dist/${args[app_template]} ./Godot.app
+/bin/cp -r misc/dist/${opts[app_template]} ./Godot.app
 /bin/mkdir -p Godot.app/Contents/MacOS
-/bin/cp bin/${args[app_bin]}.${args[arch]} Godot.app/Contents/MacOS/Godot
+/bin/cp bin/${opts[app_bin]}.${opts[arch]} Godot.app/Contents/MacOS/Godot
 /bin/chmod +x Godot.app/Contents/MacOS/Godot 
-codesign --force --timestamp --options=runtime --entitlements misc/dist/${args[platform]}/editor.entitlements -s - Godot.app
+codesign --force --timestamp --options=runtime --entitlements misc/dist/${opts[platform]}/editor.entitlements -s - Godot.app
 
 # revert enabling ccache on Godot
-if [[ ${args[ccache]} == "true" ]]; then
-  git checkout platform/${args[platform]}/detect.py
+if [[ ${opts[ccache]} == "true" ]]; then
+  git checkout platform/${opts[platform]}/detect.py
 fi
 popd
 
