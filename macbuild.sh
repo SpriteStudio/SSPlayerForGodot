e140824628eec4bb245dd4f2dec353423fe056ab
Naruto TAKAHASHI <tnaruto@gmail.com>
2024-05-14 23:50:48 +0900

    change option's separate chacter from : to =

--- macbuild.sh
+++ macbuild.sh
@@ -10,9 +10,10 @@ declare -A opts=(
     # Godot Build Options
     ["arch"]=${HOST_ARCH}
     ["scons_opts"]=""
+    ["tests"]="no"
 
     # Build Options
-    ["ccache"]="false"
+    ["ccache"]="no"
     ["version"]="3.x"
 )
 
@@ -20,19 +21,20 @@ APP=$(basename $0)
 func usage() {
     echo "Usage: $APP [options]"
     echo "Options:"
-    echo "  arch:<arch>         Target architecture (default: ${HOST_ARCH})"
-    echo "  ccache:<true|false> Enable ccache (default: false)"
-    echo "  version:<version>   Godot version. $APP uses this version at can not getting Godot version from git branch or tag. (default: 3.x)"
+    echo "  arch=<arch>         Target architecture (default: ${HOST_ARCH})"
+    echo "  tests=<yes|no>      Enable unit tests for only 4.2 (default: no)"
+    echo "  ccache=<yes|no>     Enable ccache (default: no)"
+    echo "  version=<version>   Godot version. $APP uses this version at can not getting Godot version from git branch or tag. (default: 3.x)"
 }
 
 while (( $# > 0 )); do
     item="$1"
     shift
 
-    if [[ $item = *":"* ]]; then
-        kv=(${(@s/:/)item})
+    if [[ $item = *"="* ]]; then
+        kv=(${(@s/=/)item})
         key=$kv[1]
-        value=$kv[2]
+        value=${kv[2]:l}
         opts[$key]=$value
     elif [[ $item = *"help"* || $item == "-h" || $item == "--h" ]]; then
         usage
@@ -47,7 +49,7 @@ done
 echo ""
 
 # ccache
-if [[ ${opts[ccache]} == "true" ]]; then
+if [[ ${opts[ccache]} == "yes" ]]; then
     if ! declare -p CCACHE > /dev/null 2>&1; then
         if type "sccache" > /dev/null; then
             export CCACHE=sccache
@@ -75,14 +77,18 @@ if [[ ${VERSION} =~ ^3\..* ]]; then
     opts[platform]="osx"
     opts[app_template]="osx_tools.app"
     opts[app_bin]="godot.osx.tools"
+
+    scons_command_opts="platform=${opts[platform]} compiledb=yes custom_modules='../gd_spritestudio' ${opts[scons_opts]}"
 else
     # 4.x
     opts[platform]="macos"
     opts[app_template]="macos_tools.app"
     opts[app_bin]="godot.macos.editor"
+
+    scons_command_opts="platform=${opts[platform]} compiledb=yes custom_modules='../gd_spritestudio' tests=${opts[tests]} ${opts[scons_opts]}"
 fi
 
-if [[ ${opts[arch]:l} == "universal" ]]; then
+if [[ ${opts[arch]} == "universal" ]]; then
     ARCHES=('arm64' 'x86_64')
 else
     ARCHES=${opts[arch]}
@@ -90,21 +96,21 @@ fi
 
 pushd godot
 # enable ccache on Godot
-if [[ ${opts[ccache]} == "true" ]]; then
+if [[ ${opts[ccache]} == "yes" ]]; then
     git checkout platform/${opts[platform]}/detect.py
     git apply ../misc/ccache/${opts[platform]}_ccache.patch
 fi
 
+alias scons_macro="scons ${scons_command_opts}"
 for arch in $ARCHES; do
     echo "build target arch: $arch"
-    echo "${opts[scons_opts]}"
-    scons platform=${opts[platform]} arch=${arch} compiledb=yes custom_modules="../gd_spritestudio" ${opts[scons_opts]}
+    scons_macro arch=$arch
 done
 
 /bin/rm -rf ./Godot.app
 /bin/cp -r misc/dist/${opts[app_template]} ./Godot.app
 /bin/mkdir -p Godot.app/Contents/MacOS
-if [[ ${opts[arch]:l} == "universal" ]]; then
+if [[ ${opts[arch]} == "universal" ]]; then
     lipo -create bin/${opts[app_bin]}.arm64 bin/${opts[app_bin]}.x86_64 -output bin/${opts[app_bin]}.${opts[arch]}
 fi
 /bin/cp bin/${opts[app_bin]}.${opts[arch]} Godot.app/Contents/MacOS/Godot
@@ -112,7 +118,7 @@ fi
 codesign --force --timestamp --options=runtime --entitlements misc/dist/${opts[platform]}/editor.entitlements -s - Godot.app
 
 # revert enabling ccache on Godot
-if [[ ${opts[ccache]} == "true" ]]; then
+if [[ ${opts[ccache]} == "yes" ]]; then
     git checkout platform/${opts[platform]}/detect.py
 fi
 popd
