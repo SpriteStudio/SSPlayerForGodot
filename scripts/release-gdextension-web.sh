fdf8d83453da522edc186977d65e653be81aa6bc
Naruto TAKAHASHI <tnaruto@gmail.com>
2025-04-15 22:04:27 +0900

    add release-gdextension-web.sh and release-gdextension-linux.sh

---
+++ scripts/release-gdextension-web.sh
@@ -0,0 +1,16 @@
+#/bin/zsh -e
+
+BASEDIR=$(dirname $0)
+BASEDIR=$(cd $BASEDIR && pwd -P)
+ROOTDIR=${BASEDIR}/..
+ROOTDIR=$(cd $ROOTDIR && pwd -P)
+
+pushd ${ROOTDIR} > /dev/null
+
+targets=("template_release" "template_debug")
+for target in ${targets[@]}; do
+    scripts/build-extension.sh platform=web arch=wasm32 strip=yes target=${target}
+    scripts/build-extension.sh platform=web arch=wasm32 strip=yes target=${target} threads=no
+done
+
+popd > /dev/null # ${ROOTDIR}
