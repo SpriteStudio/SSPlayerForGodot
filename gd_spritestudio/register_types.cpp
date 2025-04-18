646ead753f0a21c8368a47f95c41c7b0b74bd63e
Naruto TAKAHASHI <tnaruto@gmail.com>
2025-04-18 23:58:22 +0900

    add spritestudio_godot_library_init

--- gd_spritestudio/register_types.cpp
+++ gd_spritestudio/register_types.cpp
@@ -5,6 +5,7 @@
 #include "register_types.h"
 
 #ifdef SPRITESTUDIO_GODOT_EXTENSION
+#include <godot_cpp/godot.hpp>
 #include <godot_cpp/core/class_db.hpp>
 using namespace godot;
 #else
@@ -162,7 +163,7 @@ void unregister_gd_spritestudio_types()
 	s_LoaderSsProject.unref();
 }
 
-#ifdef GD_V4
+#if defined(GD_V4) || defined(SPRITESTUDIO_GODOT_EXTENSION)
 void initialize_gd_spritestudio_module( ModuleInitializationLevel p_level )
 {
 	if ( p_level != MODULE_INITIALIZATION_LEVEL_SCENE ) {
@@ -179,5 +180,15 @@ void uninitialize_gd_spritestudio_module( ModuleInitializationLevel p_level )
 	}
 
 	unregister_gd_spritestudio_types();
+}
+#endif
+
+#ifdef SPRITESTUDIO_GODOT_EXTENSION
+extern "C" GDExtensionBool GDE_EXPORT spritestudio_godot_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
+	godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
+	init_obj.register_initializer(initialize_gd_spritestudio_module);
+	init_obj.register_terminator(uninitialize_gd_spritestudio_module);
+	init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
+	return init_obj.init();
 }
 #endif
