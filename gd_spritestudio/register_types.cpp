af78a4424c1a623827b5900c2cf88eb137ba3997
Naruto TAKAHASHI <tnaruto@gmail.com>
2026-01-23 22:59:59 +0900

    add ssab Resource

--- gd_spritestudio/register_types.cpp
+++ gd_spritestudio/register_types.cpp
@@ -14,33 +14,58 @@ using namespace godot;
 #include "core/object/class_db.h"
 #endif
 
-void register_gd_spritestudio_types()
-{
+#include "gd_resource_ssab.h"
+static GdResourceSsabResourceFormatLoader *ssab_loader;
+static GdResourceSsabResourceFormatSaver *ssab_saver;
+
+void register_gd_spritestudio_types() {
+
+#ifndef SPRITESTUDIO_GODOT_EXTENSION
+    ClassDB::register_class<GdResourceSsabResourceFormatLoader>();
+    ClassDB::register_class<GdResourceSsabResourceFormatSaver>();
+#endif
+
+#ifdef SPRITESTUDIO_GODOT_EXTENSION
+    ssab_loader = memnew(GdResourceSsabResourceFormatLoader);
+    ResourceLoader::get_singleton()->add_resource_format_loader(ssab_loader);
+
+    ssab_saver = memnew(GdResourceSsabResourceFormatSaver);
+    ResourceSaver::get_singleton()->add_resource_format_saver(ssab_saver);
+#else
+    ssab_loader = memnew(GdResourceSsabResourceFormatLoader);
+    ResourceLoader::add_resource_format_loader(ssab_loader);
+
+    ssab_saver = memnew(GdResourceSsabResourceFormatSaver);
+    ResourceSaver::add_resource_format_saver(ssab_saver);
+#endif
+
 }
 
-void unregister_gd_spritestudio_types()
-{
+void unregister_gd_spritestudio_types() {
+#ifdef SPRITESTUDIO_GODOT_EXTENSION
+    ResourceLoader::get_singleton()->remove_resource_format_loader(ssab_loader);
+    ResourceSaver::get_singleton()->remove_resource_format_saver(ssab_saver);
+#else
+    ResourceLoader::remove_resource_format_loader(ssab_loader);
+    ResourceSaver::remove_resource_format_saver(ssab_saver);
+#endif
 }
 
-#if defined(GD_V4) || defined(SPRITESTUDIO_GODOT_EXTENSION)
-void initialize_gd_spritestudio_module( ModuleInitializationLevel p_level )
-{
-	if ( p_level != MODULE_INITIALIZATION_LEVEL_SCENE ) {
-		return;
-	}
-
+void initialize_gd_spritestudio_module(ModuleInitializationLevel level) {
+	if (level == MODULE_INITIALIZATION_LEVEL_CORE) {
+        ClassDB::register_class<GdResourceSsabResourceFormatLoader>();
+        ClassDB::register_class<GdResourceSsabResourceFormatSaver>();
+	    return;        
+    }
+    if (level != MODULE_INITIALIZATION_LEVEL_SCENE) return;
 	register_gd_spritestudio_types();
 }
 
-void uninitialize_gd_spritestudio_module( ModuleInitializationLevel p_level )
-{
-	if ( p_level != MODULE_INITIALIZATION_LEVEL_SCENE ) {
-		return;
-	}
-
+void uninitialize_gd_spritestudio_module(ModuleInitializationLevel level) {
 	unregister_gd_spritestudio_types();
+
+	if (level != MODULE_INITIALIZATION_LEVEL_CORE) return;
 }
-#endif
 
 #ifdef SPRITESTUDIO_GODOT_EXTENSION
 extern "C" GDExtensionBool GDE_EXPORT spritestudio_godot_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
