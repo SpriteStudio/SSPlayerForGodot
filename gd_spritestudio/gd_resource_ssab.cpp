3367f20ed22093f68371bd76f02f5df94297ce15
Naruto TAKAHASHI <tnaruto@gmail.com>
2026-01-23 22:59:59 +0900

    add ssab Resource

---
+++ gd_spritestudio/gd_resource_ssab.cpp
@@ -0,0 +1,178 @@
+
+#include "gd_resource_ssab.h"
+#ifdef SPRITESTUDIO_GODOT_EXTENSION
+#include <godot_cpp/classes/file_access.hpp>
+#include <godot_cpp/core/error_macros.hpp>
+#else
+#include "core/error/error_list.h"
+#include "core/error/error_macros.h"
+#include "core/io/file_access.h"
+#endif
+
+void GdResourceSsabResource::_bind_methods() {
+  ClassDB::bind_method(D_METHOD("load_from_file", "path"), &GdResourceSsabResource::load_from_file);
+  ClassDB::bind_method(D_METHOD("save_to_file", "path"), &GdResourceSsabResource::save_to_file);
+  ClassDB::bind_method(D_METHOD("get_animation_count"), &GdResourceSsabResource::get_animation_count);
+  ClassDB::bind_method(D_METHOD("get_animation_names"), &GdResourceSsabResource::get_animation_names);
+} 
+
+Error GdResourceSsabResource::load_from_file(const String &path) {
+  Error error = OK;
+#ifdef SPRITESTUDIO_GODOT_EXTENSION
+  binary = FileAccess::get_file_as_bytes(path);
+  if (binary.size() == 0)
+    return ERR_INVALID_DATA;
+#else
+  binary = FileAccess::get_file_as_bytes(path, &error);
+  if (error != OK)
+    return error;
+#endif
+
+  return error;
+
+	// return ERR_FILE_UNRECOGNIZED;
+}
+
+Error GdResourceSsabResource::save_to_file(const String &path) {
+  Error error;
+#ifdef SPRITESTUDIO_GODOT_EXTENSION
+  Ref<FileAccess> file = FileAccess::open(path, FileAccess::WRITE);
+  if (!file.is_valid())
+    return ERR_FILE_CANT_OPEN;
+#else
+  Ref<FileAccess> file = FileAccess::open(path, FileAccess::WRITE, &error);
+  if (error != OK)
+    return error;
+#endif
+  file->store_buffer(binary.ptr(), binary.size());
+  file->flush();
+  return OK;
+}
+
+int GdResourceSsabResource::get_animation_count() {
+  auto ss_anime_binary = this->get_ss_anime_binary();
+  return ss_anime_binary->animations()->size();
+}
+
+#ifdef SPRITESTUDIO_GODOT_EXTENSION
+PackedStringArray GdResourceSsabResource::get_animation_names() {
+    PackedStringArray vec;
+#else
+Vector<String> GdResourceSsabResource::get_animation_names() {
+    Vector<String> vec;
+#endif
+    auto ss_anime_binary = this->get_ss_anime_binary();
+    auto num  = ss_anime_binary->animations()->size();
+    for (int i=0; i < num; i++) {
+      auto animation = ss_anime_binary->animations()->Get(i);
+      auto name = animation->name();
+      vec.push_back(String(name->c_str()));
+    }
+    return vec;
+}
+
+
+const SsAnimeBinary *GdResourceSsabResource::get_ss_anime_binary() {
+  return GetSsAnimeBinary(this->binary.ptr());
+}
+
+const uint8_t *GdResourceSsabResource::get_data_ptr() {
+  return this->binary.ptr();
+}
+
+#ifndef SPRITESTUDIO_GODOT_EXTENSION
+Error GdResourceSsabResource::copy_from(const Ref<Resource> &p_resource) {
+  auto error = Resource::copy_from(p_resource);
+  if (error != OK)
+    return error;
+  const Ref<GdResourceSsabResource> &ssabFile =
+      static_cast<const Ref<GdResourceSsabResource> &>(p_resource);
+  this->binary = ssabFile->binary;
+  emit_signal(SNAME("ssab_file_changed"));
+  return OK;
+}
+#endif
+
+
+#ifdef SPRITESTUDIO_GODOT_EXTENSION
+Variant GdResourceSsabResourceFormatLoader::_load(const String &path,
+                                                 const String &original_path,
+                                                 bool use_sub_threads,
+                                                 int32_t cache_mode) {
+#else
+Ref<Resource> GdResourceSsabResourceFormatLoader::load(const String &path,
+                                                       const String &original_path,
+                                                       Error *error, bool use_sub_threads,
+                                                       float *progress,
+                                                       CacheMode cache_mode) {
+#endif
+  Ref<GdResourceSsabResource> ssab_file = memnew(GdResourceSsabResource);
+  ssab_file->load_from_file(path);
+#ifndef SPRITESTUDIO_GODOT_EXTENSION
+  if (error)
+    *error = OK;
+#endif
+  return ssab_file;
+}
+
+#ifdef SPRITESTUDIO_GODOT_EXTENSION
+PackedStringArray GdResourceSsabResourceFormatLoader::_get_recognized_extensions() {
+  PackedStringArray extensions;
+  extensions.push_back("ssab");
+  return extensions;
+}
+#else
+void GdResourceSsabResourceFormatLoader::get_recognized_extensions(List<String> *extensions) const {
+  extensions->push_back("ssab");
+}
+#endif
+
+#ifdef SPRITESTUDIO_GODOT_EXTENSION
+String GdResourceSsabResourceFormatLoader::_get_resource_type(const String &path) {
+#else
+String GdResourceSsabResourceFormatLoader::get_resource_type(const String &path) const {
+#endif
+  return path.ends_with(".ssba") ? "GdResourceSsabResource" : "";
+}
+
+#ifdef SPRITESTUDIO_GODOT_EXTENSION
+bool GdResourceSsabResourceFormatLoader::_handles_type(const StringName &type) {
+#else
+bool GdResourceSsabResourceFormatLoader::handles_type(const String &type) const {
+#endif
+  return type == StringName("GdResourceSsabResource") || ClassDB::is_parent_class(type, "GdResourceSsabResource");
+}
+
+#ifdef SPRITESTUDIO_GODOT_EXTENSION
+Error GdResourceSsabResourceFormatSaver::_save(const Ref<Resource> &resource, const String &path, uint32_t flags) {
+#else
+Error GdResourceSsabResourceFormatSaver::save(const Ref<Resource> &resource, const String &path, uint32_t flags) {
+#endif
+  Ref<GdResourceSsabResource> res = resource;
+  Error error = res->save_to_file(path);
+  return error;
+}
+
+#ifdef SPRITESTUDIO_GODOT_EXTENSION
+PackedStringArray GdResourceSsabResourceFormatSaver::_get_recognized_extensions(const Ref<Resource> &resource) {
+  PackedStringArray extensions;
+  if (Object::cast_to<GdResourceSsabResource>(*resource)) {
+    extensions.push_back("bssab");
+  }
+  return extensions;
+}
+#else
+void GdResourceSsabResourceFormatSaver::get_recognized_extensions(const Ref<Resource> &resource, List<String> *p_extensions) const {
+  if (Object::cast_to<GdResourceSsabResource>(*resource)) {
+    p_extensions->push_back("bssab");
+  }
+}
+#endif
+
+#ifdef SPRITESTUDIO_GODOT_EXTENSION
+bool GdResourceSsabResourceFormatSaver::_recognize(const Ref<Resource> &resource) {
+#else
+bool GdResourceSsabResourceFormatSaver::recognize(const Ref<Resource> &resource) const {
+#endif
+  return Object::cast_to<GdResourceSsabResource>(*resource) != nullptr;
+}
--- gd_spritestudio/register_types.h
+++ gd_spritestudio/register_types.h
@@ -10,16 +10,14 @@
 #ifdef SPRITESTUDIO_GODOT_EXTENSION
 #include <godot_cpp/godot.hpp>
 using namespace godot;
-#elif defined(GD_V4)
+#else
 #include "modules/register_module_types.h"
 #endif
 
 void register_gd_spritestudio_types();
 void unregister_gd_spritestudio_types();
 
-#if defined(GD_V4) || defined(SPRITESTUDIO_GODOT_EXTENSION)
 void initialize_gd_spritestudio_module( ModuleInitializationLevel p_level );
 void uninitialize_gd_spritestudio_module( ModuleInitializationLevel p_level );
-#endif
 
 #endif // GD_SPRITESTUDIO_REGISTER_TYPES_H
