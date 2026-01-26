4804090f0f37039d0a7849bd0ae53523a7b389c9
Naruto TAKAHASHI <tnaruto@gmail.com>
2026-01-26 22:15:43 +0900

    rename a converter tab name.

--- gd_spritestudio/gd_ss_editor_plugin.cpp
+++ gd_spritestudio/gd_ss_editor_plugin.cpp
@@ -10,7 +10,7 @@ GdSsEditorPlugin::GdSsEditorPlugin() {
 #include "editor/editor_node.h"
 GdSsEditorPlugin::GdSsEditorPlugin(EditorNode *node) {
     this->import_dock = memnew(GdSsImportControl);
-    import_dock->set_name(String::utf8("SSPJ Importer"));
+    import_dock->set_name(String::utf8("SSPJ"));
     add_control_to_dock(DOCK_SLOT_RIGHT_UL, this->import_dock, Ref<Shortcut>());
 }
 
